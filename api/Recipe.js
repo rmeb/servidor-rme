1a58cac87260b2297ea62b1ed1e799216bd47b5c
coloro <colorolabs@gmail.com>
2018-07-01 18:23:10 -0400

    save and get recipe

--- api/Recipe.js
+++ api/Recipe.js
@@ -1,4 +1,8 @@
 const {success, fail, error} = require('../utils/Reply')
+const db = require('../db/Postgresql')
+
+const SAVE = 'INSERT INTO recipes (id, xml) VALUES ($1, $2)'
+const GET = 'SELECT xml FROM recipes WHERE id = $1'
 
 function saveRecipe(req, res) {
   console.log('[Recipe.saveRecipe]', req.body)
@@ -11,11 +15,16 @@ function saveRecipe(req, res) {
     return fail(res, 'Parameter missing or invalid')
   }
 
-  let data = {
-    receta, id
-  }
-
-  success(res, 'Ok.')
+  db.query(SAVE, [id, receta]).then(result => {
+    console.log('[Recipe.saveRecipe] Receta guardada.')
+    success(res, 'Ok.')
+  }).catch(e => {
+    console.error('[ERROR][Recipe.saveRecipe] ' + e.code + '::' + e.detail)
+    if (e.code === '23505') {
+      return fail(res, 'El id especificado ya existe')
+    }
+    error(res, 'Error Interno ' + e.code)
+  })
 }
 
 function getRecipe(req, res) {
@@ -26,8 +35,16 @@ function getRecipe(req, res) {
     return fail(res, 'Parameter missing or invalid')
   }
 
-  success(res, {
-    receta: '<?xml>'
+  db.query(GET, [id]).then(result => {
+    if (result.rows.length === 0) {
+      return fail(res, 'Receta not found', 404)
+    }
+    success(res, {
+      receta: result.rows[0].xml
+    })
+  }).catch(e => {
+    console.error('[ERROR][Recipe.saveRecipe] ' + e.code + '::' + e.detail)
+    error(res, 'Error Interno ' + e.code)
   })
 }
 
